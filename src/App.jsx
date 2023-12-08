f6b822906e13d58383a4e9c6d002ac65c580da9b
EddMoi06 <145163602+EddMoi06@users.noreply.github.com>
2023-12-08 16:56:10 -0400

    new file:   .env 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/AppClima.jsx 	new file:   src/components/Formulario.jsx 	new file:   src/context/ClimaProvider.jsx 	new file:   src/hooks/useClima.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { ClimaProvider } from "./context/ClimaProvider"
+import AppClima from "./components/AppClima"
 
 function App() {
 
   return (
     <>
-      
+      <ClimaProvider>
+        <header>
+          <h1>Buscador de Clima</h1>
+        </header>
+        <AppClima/>
+      </ClimaProvider>
     </>
   )
 }
---
+++ src/hooks/useClima.jsx
@@ -0,0 +1,9 @@
+import ClimaContext from "../context/ClimaProvider";
+import { useContext } from 'react'
+
+
+const useClima = () => {
+    return useContext(ClimaContext)
+}
+
+export default useClima
